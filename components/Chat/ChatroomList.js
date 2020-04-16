550d019039946dabc711425150ace5ed0786269c
CHu <c>
2020-04-16 23:16:27 +0800

    [New] Complete get userinfo from firebase and applied to chatroom list

--- components/Chat/ChatroomList.js
+++ components/Chat/ChatroomList.js
@@ -1,82 +1,21 @@
-import React, { Component } from 'react';
-import { Text, View, TextInput, FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements';
 
+import React, { Component } from 'react';
+import { Text, View, TextInput, FlatList, SafeAreaView, Dimensions } from 'react-native';
+import { ListItem, Button } from 'react-native-elements';
+import { db } from '../../config/config';
+
+const windowWidth = Dimensions.get('window').width;
   
 export default class ChatroomList extends Component {
   constructor(props) {
     super(props)
   }
-  state = {
-    list : [
-      {
-        name: 'Amy Farha',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-        subtitle: 'Vice President',
-        id: 1
-      },
-      {
-        name: 'Chris Jackson',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-        subtitle: 'Vice Chairman',
-        id: 2
-      },
-      {
-        name: 'Amy Farha',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-        subtitle: 'Vice President',
-        id: 3
-      },
-      {
-        name: 'Chris Jackson',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-        subtitle: 'Vice Chairman',
-        id: 4
-      },
-      {
-        name: 'Amy Farha',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-        subtitle: 'Vice President',
-        id: 5
-      },
-      {
-        name: 'Chris Jackson',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-        subtitle: 'Vice Chairman',
-        id: 6
-      },
-      {
-        name: 'Amy Farha',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-        subtitle: 'Vice President',
-        id: 7
-      },
-      {
-        name: 'Chris Jackson',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-        subtitle: 'Vice Chairman',
-        id: 8
-      },
-      {
-        name: 'Amy Farha',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-        subtitle: 'Vice President',
-        id: 9
-      },
-      {
-        name: 'Chris Jackson',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-        subtitle: 'Vice Chairman',
-        id: 10
-      },
-    ]
-  }
     keyExtractor = (item, index) => index.toString()
-
+    
     renderItem = ({ item }) => (
       <ListItem
         title={item.name}
-        subtitle={item.subtitle}
+        //subtitle={item.subtitle}
         leftAvatar={{ source: { uri: item.avatar_url } }}
         bottomDivider
         chevron
@@ -86,11 +25,15 @@ export default class ChatroomList extends Component {
 
   render() {
     return (
+      <SafeAreaView>
         <FlatList
           keyExtractor={this.keyExtractor}
+          //data={this.props.chatList}
           data={this.props.chatList}
           renderItem={this.renderItem}
+          style={{width:windowWidth}}
         />
+      </SafeAreaView>
     );
   }   
 } 
--- components/Chat/ChatroomListPage.js
+++ components/Chat/ChatroomListPage.js
@@ -1,103 +1,36 @@
 import React from 'react'
-import { View, Text, Button, Flatlist, ScrollView } from 'react-native'
+import { View, Text, Button, FlatList, ScrollView, SafeAreaView } from 'react-native'
 import { ListItem } from 'react-native-elements';
 
 import Chatroom from './Chatroom'
 import ChatroomList from './ChatroomList'
+import { db } from '../../config/config';
+
 
 function keyExtractor(item, index){
     return index.toString()
 }
-
-function renderItem ({ item }){
-    return(
-    <ListItem
-        title={item.name}
-        subtitle={item.subtitle}
-        leftAvatar={{ source: { uri: item.avatar_url } }}
-        bottomDivider
-        chevron
-    />
-    )
-}
-function onClickChatRoomUser(id){
-  alert("onClickChatRoomUser: "+id);
- 
-}
 export function ChatroomListPage({ route, navigation }) {
-    const list = [
-        
-      {
-        name: 'Amy Farha',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-        subtitle: 'Vice President',
-        id: 1
-      },
-      {
-        name: 'Chris Jackson',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-        subtitle: 'Vice Chairman',
-        id: 2
-      },
-      {
-        name: 'Amy Farha',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-        subtitle: 'Vice President',
-        id: 3
-      },
-      {
-        name: 'Chris Jackson',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-        subtitle: 'Vice Chairman',
-        id: 4
-      },
-      {
-        name: 'Amy Farha',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-        subtitle: 'Vice President',
-        id: 5
-      },
-      {
-        name: 'Chris Jackson',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-        subtitle: 'Vice Chairman',
-        id: 6
-      },
-      {
-        name: 'Amy Farha',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-        subtitle: 'Vice President',
-        id: 7
-      },
-      {
-        name: 'Chris Jackson',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-        subtitle: 'Vice Chairman',
-        id: 8
-      },
-      {
-        name: 'Amy Farha',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-        subtitle: 'Vice President',
-        id: 9
-      },
-      {
-        name: 'Chris Jackson',
-        avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-        subtitle: 'Vice Chairman',
-        id: 10
-      },
-        // more items
-    ]
+  let userInfo = []
+  
+  db.ref('/users').once('value',  snap => {
+    console.log(snap);
+    snap.forEach(
+        child => {
+          userInfo.push(child.val());
+        }
+    )
+  })
     return (
-      <View style={{  flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <SafeAreaView style={{  flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Chat Screen</Text>
-  
-        <ChatroomList chatList={list} onClickChatRoomUser={(item)=> navigation.navigate('Chatroom',{ item: item})}/>
-  
+        <ChatroomList 
+        chatList={userInfo} 
+        onClickChatRoomUser={(item)=> navigation.navigate('Chatroom',{ item: item})}
+        />
   
         <Button title="Go to Chatroom" onPress={() => navigation.navigate('Chatroom')} />
-      </View>
+      </SafeAreaView>
       
     );
 }
