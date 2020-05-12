2eaa533df664365043ba1abf3654fae8e0b7cd2e
VictorYip <victoryip1234@gmail.com>
2020-05-12 20:23:39 +0800

    [Update] Profile fetch real-time data

--- components/Profile/ProfileScreen.js
+++ components/Profile/ProfileScreen.js
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ScrollView, View,Text, Button, Image, StyleSheet, FlatList, ActivityIndicator, Dimensions } from 'react-native'
 import { db, storage } from '../../config/config';
 
 var backgroundImage = require('../../assets/wallpaper.jpg');
 var num_of_followers = '5215';
 var num_of_views = '154420';
+var loading = true ;
+var user = false;
 
 const windowHeight = Dimensions.get('window').height;
+
+console.disableYellowBox = true;
+console.ignoredYellowBox = ['Setting a timer'];
 
 const styles = StyleSheet.create({
     loading: {
@@ -65,52 +70,155 @@ const styles = StyleSheet.create({
         paddingTop: 30,
     }
 
-  });  
+  }); 
+
+function getUserData(_id) {
+    let userData = [];
+
+    db.ref('/users/' + _id).once('value',  snap => {
+        //console.log(snap.val()['name']);
+        userData.push(snap.val()['name']);
+        userData.push(snap.val()['Description']);
+    });
+
+    return userData;
+}
+
+async function downloadImg (ref) {
+    let imageLink = await ref.getDownloadURL();
+    return imageLink.toString() 
+} 
+
+
+async function getAssetImage(_id) {
+    const icon_ref = storage.ref('user/' + _id + '/icon/icon.jpg');
+    const wallpaper_ref = storage.ref('user/' + _id + '/wallpaper/wallpaper.jpg');
+    const promises = [];
+    
+    promises.push(downloadImg(wallpaper_ref));
+    promises.push(downloadImg(icon_ref));
+
+    return Promise.all(promises);
+}
+
+async function getGalleryImage(_id) {
+    const gallery_ref = storage.ref('/user/' + _id + '/gallery');
+    const promises = [];
+
+    await gallery_ref.listAll().then(result=> {
+        result.items.forEach( imageRef=> {
+            promises.push(downloadImg(imageRef));
+        });
+    })
+
+    return Promise.all(promises);
+}
+
+async function loadData(user_id) {
+    const assetArray = []
+    const galleryArray = [];
+
+    console.log('Loading User Data...')
+
+    let userData = getUserData(user_id);
+
+    console.log(userData[0]);
+    
+    console.log('Loading Icon...')
+
+    await getAssetImage(user_id).then( result => {
+        result.forEach( link => {
+            assetArray.push(link);
+        })
+    });
+
+    console.log('Loading Gallery...')
+
+    await getGalleryImage(user_id).then( result => {
+        let counter = 0;
+
+        result.forEach( link => {
+            galleryArray.push({image: {uri: link}, id : counter});
+            counter ++;
+        })
+    });
+
+    console.log('-- Finished Loading -- ');
+
+    return [true, assetArray, galleryArray, userData];
+}
 
 
 export const ProfileScreen = ({ route, navigation }) => {
 
-    const [icon, setIcon] = useState('');
-    const [fetch, setFetch] = useState(false);
+    console.disableYellowBox = true;
+    console.ignoredYellowBox = ['Setting a timer'];
 
     var user_id = 3;
-    var icon_ref = storage.ref('user/' + user_id + '/icon/icon.jpg');
-    var gallery_ref = storage.ref('user/' + user_id + '/gallery/');
 
-    icon_ref.getDownloadURL().then( url => {
-        setIcon(url.toString());
-        setFetch(true);
-    })
+    //const user_id = [route.params?id];
 
-    gallery_ref.listAll().then( result => {
-        console.log(result.getDownloadURL().toString());
-        result.listAll().then ( gallery => {
-            console.log(gallery.getDownloadURL().toString());
+    const [data, setData] = useState({
+        loaded: false,
+        wallpaper: '',
+        icon: '',
+        gallery: [],
+        detail: '',
+    });
+    
+    if(loading) {
+        loadData(user_id).then( result => {
+            loading = false;
+            setData({
+                loaded: result[0],
+                wallpaper: result[1][0],
+                icon: result[1][1],
+                gallery: result[2],
+                detail: result[3],
+            })
         })
-    })
+    }
 
     return (
         <ScrollView>
-        {   fetch == false?
+        {    !data['loaded']?
                 <View style={styles.loading}>
-                    <ActivityIndicator size="large" color="#0000ff" />
+                    <ActivityIndicator size="large" color="#FF6600" />
+                    <Text> LOADING . . . </Text>
                 </View>
             :
                 <View style={styles.contentContainer}>
                     <Image
                         style={styles.jumbotron}
-                        source={backgroundImage}
+                        source={{
+                            uri: data['wallpaper']
+                        }}
                     />
                     <Image 
                         style={styles.icon} 
                         source={{
-                            uri: icon
+                            uri: data['icon']
                         }}
                     />
-                    <Text style={styles.title}> {route.params?.name} </Text>
-                    <Text style={styles.description}> {route.params?.description} </Text>
+                    <Text style={styles.title}> {data['detail'][0]} </Text>
+                    <Text style={styles.description}> {data['detail'][1]} </Text>
                     <Text style={styles.audience}>{num_of_followers} followers | {num_of_views} views</Text>
-                    <View style={styles.buttonContainer}>
+                    {   
+                        user?
+                        <View style={styles.buttonContainer}>
+                        <Button
+                            title="UpLoad Image"
+                            style={styles.button}
+                            onPress={() => navigation.navigate('Chat')}
+                        />
+                        <Button
+                            title="Chat with Friends"
+                            style={styles.button}
+                            onPress={() => navigation.navigate('Chat')}
+                        />
+                        </View>
+                        :
+                        <View style={styles.buttonContainer}>
                         <Button
                             title="Add Friend"
                             style={styles.button}
@@ -121,20 +229,11 @@ export const ProfileScreen = ({ route, navigation }) => {
                             style={styles.button}
                             onPress={() => navigation.navigate('Chat')}
                         />
-                    </View>
+                        </View>
+                    }   
                     <View style={styles.gallery}>
                         <FlatList
-                            data = {[
-                                {image: require('../../assets/gallery1.jpg'), id: '1'},
-                                {image: require('../../assets/gallery2.jpg'), id: '2'},
-                                {image: require('../../assets/gallery3.jpg'), id: '3'},
-                                {image: require('../../assets/gallery4.jpg'), id: '4'},
-                                {image: require('../../assets/gallery5.jpg'), id: '5'},
-                                {image: require('../../assets/gallery6.jpg'), id: '6'},
-                                {image: require('../../assets/gallery7.jpg'), id: '7'},
-                                {image: require('../../assets/gallery8.jpg'), id: '8'},
-                                {image: require('../../assets/gallery9.jpg'), id: '9'},
-                            ]}
+                            data = {data['gallery']}
                             keyExtractor={(item) => item.id}
                             numColumns={3}
 
@@ -151,5 +250,5 @@ export const ProfileScreen = ({ route, navigation }) => {
                 </View>
         }
         </ScrollView>
-    );
-}
+        
+    )};
